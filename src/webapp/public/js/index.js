7864e7236c802a4137682f515e4ece3381198d34
Mateusz Rorat <mateuszrorat@gmail.com>
2022-06-19 20:46:23 +0200

    frontend improvemnents

--- src/webapp/public/js/index.js
+++ src/webapp/public/js/index.js
@@ -1,12 +1,41 @@
+let search;
+let books;
+
 fetch('/api/books')
   .then((response) => response.json())
   .then((data) => {
-    data.forEach((element) => {
-      const { storage } = element;
+    books = data;
+    display();
+  });
+
+$('.search').on('keyup', (event) => {
+  search = event.originalEvent.target.value;
+  display();
+});
+
+function display() {
+  $('.books').empty();
+  books
+    .filter(
+      (element) =>
+        !search ||
+        element.ocr[0].description.toUpperCase().includes(search.toUpperCase())
+    )
+    .forEach((element) => {
+      const { storage, ocr } = element;
       $('.books').append(
         $(`
-            <div> ${storage.name} <img height="100" src='${storage.mediaLink}'/></div>
-        `)
+            <div>
+                <h3>${storage.name} </h3> 
+                <div style="display: flex">
+                    <img height="100" src='${storage.mediaLink}'/> 
+                    <div> ${ocr[0].description.replace(
+                      new RegExp('(' + search + ')', 'ig'),
+                      '<span style="background: yellow">$1</span>'
+                    )}</div>
+                </div>
+            </div>
+      `)
       );
     });
-  });
+}
